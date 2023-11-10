15874bc0f5a24171edf1e1ce0cf2c6ea1012f847
SergiEZ <klsergi@gmail.com>
2023-11-10 20:37:48 +0100

    clas 10

---
+++ u01/Actividad13-3/Actividad13-1/Form1.vb
@@ -0,0 +1,94 @@
+﻿Public Class Form1
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.Control AndAlso e.KeyCode = Keys.B Then
+            btBackground.PerformClick()
+        End If
+        If e.Control AndAlso e.KeyCode = Keys.C Then
+            btFont.PerformClick()
+        End If
+        If e.Alt AndAlso e.KeyCode = Keys.F4 Then
+            btQuit.PerformClick()
+        End If
+        If e.Control AndAlso e.Shift AndAlso e.KeyCode = Keys.A Then
+            AboutToolStripMenuItem.PerformClick()
+        End If
+        If e.Control AndAlso e.KeyCode = Keys.F1 Then
+            HelpToolStripMenuItem.PerformClick()
+        End If
+    End Sub
+    Private Sub btQuit_Click(sender As Object, e As EventArgs) Handles btQuit.Click, QuitToolStripMenuItem.Click
+        Close()
+    End Sub
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+        Try
+            Dim keyAscii As Integer = Asc(e.KeyChar)
+
+            If Asc(e.KeyChar) >= 48 And Asc(e.KeyChar) <= 57 Then ' Comprobar si es un número
+                If rbNumbers.Checked = True Or rbAll.Checked = True Then
+                    lbN.Show()
+                    lbN.Text = Chr(keyAscii)
+                    lbU.Hide()
+                    lbL.Hide()
+                    lbLower.Hide()
+                    lbUpper.Hide()
+                    lbNumber.Show()
+                End If
+            End If
+
+            If rbLetters.Checked = True Or rbAll.Checked = True Then
+                Dim tecla As String = Chr(keyAscii)
+
+                If Asc(tecla) >= 65 And Asc(tecla) <= 90 Then ' Letra mayúscula
+                    lbU.Text = tecla
+                    lbL.Text = Mid(tecla, 1, 1)
+                    lbN.Hide()
+                    lbNumber.Hide()
+                    lbU.Show()
+                    lbUpper.Show()
+                    lbL.Show()
+                    lbLower.Show()
+                ElseIf Asc(tecla) >= 97 And Asc(tecla) <= 122 Then ' Letra minúscula
+                    lbNumber.Hide()
+                    lbN.Hide()
+                    lbL.Text = tecla
+                    lbU.Text = Mid(tecla, 1, 1)
+                    lbUpper.Hide()
+                    lbLower.Hide()
+                    lbU.Show()
+                    lbUpper.Show()
+                    lbL.Show()
+                    lbLower.Show()
+                End If
+            End If
+        Catch ex As Exception
+            ' Manejar la excepción aquí
+            MessageBox.Show("Se ha producido un error al procesar la tecla.")
+        End Try
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btBackground.Click, BackgroundColorToolStripMenuItem1.Click
+        If ColorDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+            Form1.ActiveForm.BackColor = ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub btFont_Click(sender As Object, e As EventArgs) Handles btFont.Click, FontColorToolStripMenuItem1.Click
+        If ColorDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+            Form1.ActiveForm.ForeColor = ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ViewHelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ViewHelpToolStripMenuItem.Click
+        formAyuda.Show()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        AboutBox1.Show()
+    End Sub
+End Class
---
+++ u01/Actividad13-3/Actividad13-1/Form2.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formAyuda
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(formAyuda))
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 12)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.ScrollBars = ScrollBars.Horizontal
+        TextBox1.Size = New Size(403, 364)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = resources.GetString("TextBox1.Text")
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(161, 397)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(88, 28)
+        Button1.TabIndex = 1
+        Button1.Text = "OK"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' formAyuda
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(427, 441)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Name = "formAyuda"
+        Text = "Actividad13 Help"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
